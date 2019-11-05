fc1c1b87fa0e787e81e89ce300b972ecadf24af6
Nirav joshi <niravjoshi87@gmail.com>
2019-11-05 16:36:23 +0530

    ''

--- index.php
+++ index.php
@@ -43,27 +43,7 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
     <script src="src/assets/libs/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript">
-        window.$zopim || (function (d, s) {
-            var z = $zopim = function (c) { z._.push(c) },
-                $ = z.s =
-                    d.createElement(s),
-                e = d.getElementsByTagName(s)[0];
-            z.set = function (o) {
-                z.set.
-                    _.push(o)
-            };
-            z._ = [];
-            z.set._ = [];
-            $.async = !0;
-            $.setAttribute("charset", "utf-8");
-            $.src = "https://v2.zopim.com/?4gucOY1MlvrcvUVJ9GClgI5JHhmy8CFq";
-            z.t = +new Date;
-            $.
-                type = "text/javascript";
-            e.parentNode.insertBefore($, e)
-        })(document, "script");
-    </script>
+    
     <!-- Sendinblue Download Form CSS (We recommend to place the below code in head tag of your website html) -->
     <style>
         @font-face {
