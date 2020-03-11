36d464c9c1cbc22d1dd45b7b8ba9fa75cb1c3531
Nirav <niravjoshi87@gmail.com>
2020-03-11 18:34:04 +0530

    ''

--- index-old-with-email.php
+++ index-old-with-email.php
@@ -35,13 +35,6 @@
     <link rel="icon" type="image/png" sizes="16x16" href="src/assets/images/favicon.png">
     <title>AdminMart admin Template - The Ultimate Multipurpose admin template</title>
     <!-- Custom CSS -->
-    <link href="src/dist/css/pandalocker.min.css" rel="stylesheet">
-    <style type="text/css">
-        .onp-sl-header{
-            font-size:24px!important;
-            font-family: Gilmer-Heavy,sans-serif;
-        }
-    </style>
     <link href="src/dist/css/style.min.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -59,7 +52,47 @@
 
         gtag('config', 'UA-121685638-2');
     </script>
-    
+    <!-- Sendinblue Download Form CSS (We recommend to place the below code in head tag of your website html) -->
+    <style>
+        @font-face {
+            font-display: block;
+            font-family: Roboto;
+            src: url(https://assets.sendinblue.com/font/Roboto/Latin/normal/normal/7529907e9eaf8ebb5220c5f9850e3811.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/normal/normal/25c678feafdc175a70922a116c9be3e7.woff) format("woff")
+        }
+
+        @font-face {
+            font-display: fallback;
+            font-family: Roboto;
+            font-weight: 600;
+            src: url(https://assets.sendinblue.com/font/Roboto/Latin/medium/normal/6e9caeeafb1f3491be3e32744bc30440.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/medium/normal/71501f0d8d5aa95960f6475d5487d4c2.woff) format("woff")
+        }
+
+        @font-face {
+            font-display: fallback;
+            font-family: Roboto;
+            font-weight: 700;
+            src: url(https://assets.sendinblue.com/font/Roboto/Latin/bold/normal/3ef7cf158f310cf752d5ad08cd0e7e60.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/bold/normal/ece3a1d82f18b60bcce0211725c476aa.woff) format("woff")
+        }
+
+        #sib-container input:-ms-input-placeholder {
+            text-align: left;
+            font-family: "Helvetica", sans-serif;
+            color: #c0ccda;
+            border-width: px;
+        }
+
+        #sib-container input::placeholder {
+            text-align: left;
+            font-family: "Helvetica", sans-serif;
+            color: #c0ccda;
+            border-width: px;
+        }
+    </style>
+    <link rel="stylesheet" href="https://assets.sendinblue.com/component/form/2ef8d8058c0694a305b0.css">
+    <link rel="stylesheet" href="https://assets.sendinblue.com/component/clickable/b056d6397f4ba3108595.css">
+    <link rel="stylesheet" href="https://assets.sendinblue.com/component/progress-indicator/f86d65a4a9331c5e2851.css">
+    <link rel="stylesheet" href="https://sibforms.com/forms/end-form/build/sib-styles.css">
+    <!-- End - Sendinblue Download Form CSS -->
 </head>
 
 <body>
@@ -280,7 +313,7 @@
 <script src="src/assets/libs/popper.js/dist/umd/popper.min.js"></script>
 <script src="src/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
 <script type="text/javascript">
-            jQuery(document).on('click','.dcount',function(){
+            jQuery(document).on('click','.downloadcount',function(){
                 jQuery('#counter').html('Loading...') ;
                 var ajax = jQuery.ajax({
                     method : 'get',
@@ -295,8 +328,6 @@
                 }) ;
             }) ;
         </script>
-<script src="src/dist/js/jquery.ui.highlight.min.js"></script>
-<script src="src/dist/js/pandalocker.js"></script>
 <script src="src/dist/js/feather.min.js"></script>
 <script type="text/javascript">
     $(function () {
@@ -311,40 +342,164 @@
     }); 
 </script>
 
-<script>
-$(document).ready(function ($) {
-   $('.ctod').sociallocker({
-    theme: 'flat',
-    facebook:{
-       appId: '127988287842860'
-    },
-    buttons:{
-       order: ["facebook-share","twitter-tweet"],
-       counters: true,
-       lazy: true
-    }
-   });
-});
-</script>
+<!-- AdminMart Download Form Modal -->
 <div class="modal fade" id="download-modal">
     <div class="modal-dialog modal-dialog-centered">
-
         <div class="modal-content">
-             <div class="modal-header">
+        
+            <!-- Modal Header -->
+            <div class="modal-header">
                 <h4 class="modal-title">Download template</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
-
+            <!-- End - Modal Header -->
+            <!-- Modal body -->
             <div class="modal-body" style="background: #f5f8fa; padding:35px;">
-                <div class="ctod text-center">
-                <h3>Thanks for sharing</h3>
-                <a href="package.zip" class="btn btn-custom dcount btn-info btn-shadow-v1 font-tofino-medium">
-                    <i data-feather="download" class="data-icon mr-2 mb-1"></i> Click to Download</a>
+                <p>To download this template just enter your email please. You will receive your download link shortly.</p>
+                <div class="simple-download d-none">
+                    <form class="simple-download-form" method="POST" action="">
+                        <p class="status alert alert-warning" role="alert" style="display: none;">Sending mail...</p>
+                        <div class="form-group">
+                            <input class="form-control emailid rounded-pill" maxlength="100" type="email" id="emailid" name="emailid" placeholder="Email Address" required />
+                        </div>
+                        <button type="submit" id="submit-btn" name="submit-btn" class="submit-btn btn btn-custom btn-info btn-shadow-v1 font-tofino-medium mb-4 py-2">Submit</button>
+                    </form>
                 </div>
+                <!-- Sendinblue Download Form (We recommend to place the below code where you want the form in your website html)  -->
+                <div class="sib-form p-0">
+                    <div id="sib-form-container" class="sib-form-container">
+                        <div id="error-message" class="sib-form-message-panel" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-width:px; border-color:#ff4949;max-width:540px; border-width:px;">
+                            <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
+                                <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
+                                    <path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z"
+                                    />
+                                </svg>
+                                <span class="sib-form-message-panel__inner-text">Opps! Something went wrong. Email not sent.</span>
+                            </div>
+                        </div>
+                        <div></div>
+                        <div id="success-message" class="sib-form-message-panel" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#085229; background-color:#e7faf0; border-radius:3px; border-width:px; border-color:#13ce66;max-width:540px; border-width:px;">
+                            <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
+                                <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
+                                    <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z"
+                                    />
+                                </svg>
+                                <span class="sib-form-message-panel__inner-text">Download link has been sent successfully in email. If you did not get an email download the template from here: <a href="https://github.com/adminmart/Adminmart-lite">https://github.com/adminmart/Adminmart-lite</a></span>
+                            </div>
+                        </div>
+                        <div></div>
+                        <div id="sib-container" class="sib-container--large sib-container--vertical" style="padding: 0;background: #f5f8fa;">
+                            <form id="sib-form" method="POST" action="https://224e9a3e.sibforms.com/serve/MUIEAExF2xvtab7XGbac9qmSOZUgE8E43GASjV7JFU1FpmTubtKtrqW8PW50ZGiY1q4Wwbyd9GTHgHpma17OndyuQoEZ3EgjRMNfhauqYNOuS9E2H4a3015k8X0LcYoL6QpTq80g99ZRot6W9YYOhik6KEVDTtMxjbTAS9LuwVf8yO6PSKD4t6lncWioQQQaB2vNLEg6yMCer3-a"
+                                data-type="subscription">
+                                <div class="form-group">
+                                    <div class="sib-input sib-form-block" style="padding: 0;">
+                                        <div class="form__entry entry_block">
+                                            <div class="form__label-row ">
+                                                <label class="entry__label" style="font-size:16px; text-align:left; font-weight:700; font-family:&quot;Helvetica&quot;, sans-serif; color:#3c4858; border-width:px;" for="FIRSTNAME" data-required="*">Name</label>
+                                                <div class="entry__field">
+                                                    <input class="input form-control" maxlength="200" type="text" id="FIRSTNAME" name="FIRSTNAME" autocomplete="off" placeholder="Full Name" data-required="true" required />
+                                                </div>
+                                            </div>
+                                            <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-width:px; border-color:#ff4949;"></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="sib-input sib-form-block" style="padding: 0;">
+                                        <div class="form__entry entry_block">
+                                            <div class="form__label-row ">
+                                                <label class="entry__label" style="font-size:16px; text-align:left; font-weight:700; font-family:&quot;Helvetica&quot;, sans-serif; color:#3c4858; border-width:px;" for="EMAIL" data-required="*">Email</label>
+                                                <div class="entry__field">
+                                                    <input class="input form-control" maxlength="200" type="email" id="EMAIL" name="EMAIL" autocomplete="off" placeholder="Email Address" data-required="true" required />
+                                                </div>
+                                            </div>
+                                            <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-width:px; border-color:#ff4949;"></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="sib-form-block" style="padding: 0; text-align: left">
+                                        <button class="sib-form-block__button sib-form-block__button-with-loader btn btn-custom btn-info btn-shadow-v1 downloadcount font-tofino-medium" style=""
+                                            form="sib-form" type="submit">
+                                            <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512">
+                                                <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"
+                                                />
+                                            </svg>
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                                <input type="text" name="email_address_check" value="" class="input--hidden">
+                                <input type="hidden" name="locale" value="en">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- End - Sendinblue Download Form -->
+                <p class="small">When this form is submitted, I will add you to my mailing list to notify you about future updates and other Bootstrapious news.</p>
+                <p class="small">Don't worry, I won't share your email with anyone. You can unsubscribe anytime.</p>
             </div>
+            <!-- End - Modal body -->
         </div>
     </div>
 </div>
+<!-- End - AdminMart Download Form Modal -->
+
+<!-- Sendinblue Download Form JS (We recommend to place the below code in footer or bottom of your website html)  -->
+<script>
+    window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
+    window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "Invalid Email. Please try again.";
+    window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank.";
+    window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
+    window.translation = {
+        common: {
+            selectedList: '{quantity} list selected',
+            selectedLists: '{quantity} lists selected'
+        }
+    };
+    var AUTOHIDE = Boolean(0);
+</script>
+<script src="https://sibforms.com/forms/end-form/build/main.js"></script>
+<script src="https://www.google.com/recaptcha/api.js?hl=en"></script>
+<!-- End - Sendinblue Download Form JS -->
+<!-- End - Sendinblue Download Form -->
+
+<!-- Email Send Custom JS -->
+<script type="text/javascript">
+
+    /*Perform AJAX on simple-download-form submit*/
+    jQuery( 'form.simple-download-form' ).on( 'submit', function( e ){
+        var p_status = jQuery( 'p.status', this );
+        p_status.fadeIn();
+
+        var emailid = jQuery( '.emailid', this ).val(); alert(emailid);
+
+        jQuery.ajax( {
+            type: 'POST',
+            dataType: 'json',
+            url: 'ajaxemail.php',
+            data: { 'emailid'   : emailid, }, success: function( data ){
+
+                if ( data.status ){
+
+                    p_status.removeClass( 'alert-warning' );
+                    p_status.addClass( 'alert-success' );
+                    alert ('Sent');
+
+                } else {
+
+                    p_status.removeClass( 'alert-warning' );
+                    p_status.addClass( 'alert-danger' );
+                    alert ('Not Sent');
+                }
+                p_status.text( data.message );
+                p_status.fadeIn();
+            }
+        });
+        e.preventDefault();
+    });
+
+</script>
 <!-- End - Email Send Custom JS -->
 
 </html>
